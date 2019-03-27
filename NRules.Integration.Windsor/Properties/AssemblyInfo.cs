eb8c8cc09e4534bf5c0c189f8c56507c929b7aef
keithbo <keith.bosacker@gmail.com>
2019-03-26 22:31:56 -0500

    Refactor .NET Sdk csproj
    Update csproj format to use newer .NET Sdk style

--- NRules.Integration.Windsor/Properties/AssemblyInfo.cs
+++ NRules.Integration.Windsor/Properties/AssemblyInfo.cs
@@ -1,17 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NRules.Integration.Windsor")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NRules.Integration.Windsor")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -20,14 +7,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5e52d368-11e7-479f-a41c-0caa506d752c")]
-
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-
-#if DEBUG
-[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
-[assembly: AssemblyConfiguration("debug")]
-#else
-[assembly: AssemblyInformationalVersion("1.0.0")]
-[assembly: AssemblyConfiguration("retail")]
-#endif
